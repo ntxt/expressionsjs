3f309c01409d9d8187c5f1bb041e370a271c338a
Piotr Tomaszewski <ptrkntxt@gmail.com>
2016-02-24 00:44:43 +0100

    tabs to spaces, simplified validation code in evaluators

--- js/renderers-html.js
+++ js/renderers-html.js
@@ -4,97 +4,94 @@ net.ntxt.expressions = net.ntxt.expressions || {};
 net.ntxt.expressions.renderers = net.ntxt.expressions.renderers || {};
 net.ntxt.expressions.renderers.html = (function html()
 {
-	var TARGET = 'html',
-	    templates = {
-		operator       : $('<span>').addClass('operator interactive'),
-		binaryArgument : $('<span>').addClass('argument interactive'),
-		expression     : $('<div>').addClass('expression'),
-		listExpression : $('<div>').addClass('list expression'),
-		list           : $('<ul>'),
-		listElement    : $('<li>'),
-		variable       : $('<span>').addClass('variable'),
-		literal        : $('<span>').addClass('literal')		
-	},
-	renderers = {
-	  'VAR-STRING'   : genericVarRenderer,
-	  'VAR-NUMBER'   : genericVarRenderer,
-	  'VAR-BOOLEAN'  : genericVarRenderer,
-	  'VAR-DATE'     : genericVarRenderer,
-	  'VAL-STRING'   : genericLiteralRenderer,
-	  'VAL-NUMBER'   : genericLiteralRenderer,	
-	  'AND'          : genericListRenderer,
-	  'EQUAL'        : genericBinaryOpRenderer,
-	  'GREATER'      : genericBinaryOpRenderer,	
-	  'LESS'         : genericBinaryOpRenderer		
-	};
-	
+    var TARGET = 'html',
+        templates = {
+        operator       : $('<span>').addClass('operator interactive'),
+        binaryArgument : $('<span>').addClass('argument interactive'),
+        expression     : $('<div>').addClass('expression'),
+        listExpression : $('<div>').addClass('list expression'),
+        list           : $('<ul>'),
+        listElement    : $('<li>'),
+        variable       : $('<span>').addClass('variable'),
+        literal        : $('<span>').addClass('literal')        
+    },
+    renderers = {
+      'VAR-STRING'   : genericVarRenderer,
+      'VAR-NUMBER'   : genericVarRenderer,
+      'VAR-BOOLEAN'  : genericVarRenderer,
+      'VAR-DATE'     : genericVarRenderer,
+      'VAL-STRING'   : genericLiteralRenderer,
+      'VAL-NUMBER'   : genericLiteralRenderer,    
+      'AND'          : genericListRenderer,
+      'EQUAL'        : genericBinaryOpRenderer,
+      'GREATER'      : genericBinaryOpRenderer,    
+      'LESS'         : genericBinaryOpRenderer        
+    };
+    
   function genericVarRenderer(ex){
-		var op = ex.op,
-				varName = ex.args[0],
-				view = templates.variable.clone()
-				 .addClass('op-'+op)
-				 .text(varName);
-		return view;
-	};
-	
-	function genericLiteralRenderer(ex, target){
-		var op = ex.op,
-				varName = ex.args[0],
-				view = templates.literal.clone()
-				 .addClass('op-'+op)
-				 .text(varName);
-		return view;
-	};
-	
-	function genericListRenderer(ex, target){
-		var self = this,
-		    op = ex.op,
-				view = templates.listExpression.clone(),
-				header = templates.operator.clone(),
-				list = templates.list.clone();
-				
-		view.addClass('op-'+op)
-		    .append(header)
-				.append(list);
-				
-//		alert('operators are not accessible in the renderer provider');
-//		throw('here');
-		
-		header.text(self.operators(op).label);
-		$.each(ex.args, function(i, arg){
-			var argView = templates.listElement.clone().append(self.render(arg, target));
-			list.append(argView);											 
-		});
-		return view;
+        var op = ex.op,
+            varName = ex.args[0],
+            view = templates.variable.clone()
+                .addClass('op-'+op)
+                .text(varName);
+        return view;
+    };
+    
+    function genericLiteralRenderer(ex, target){
+        var op = ex.op,
+            varName = ex.args[0],
+            view = templates.literal.clone()
+                .addClass('op-'+op)
+                .text(varName);
+        return view;
+    };
+    
+    function genericListRenderer(ex, target){
+        var self = this,
+            op = ex.op,
+            view = templates.listExpression.clone(),
+            header = templates.operator.clone(),
+            list = templates.list.clone();
+                
+        view.addClass('op-'+op)
+            .append(header)
+                .append(list);
+        
+        header.text(self.operators(op).label);
+        $.each(ex.args, function(i, arg){
+            var argView = templates.listElement.clone().append(self.render(arg, target));
+            list.append(argView);                                             
+        });
+        return view;
   };
-	
-	function genericBinaryOpRenderer(ex, target){
-		var self = this,
-		    op = ex.op,
-				view = templates.expression.clone(),
-				arg1 = ex.args[0],
-				arg2 = ex.args[1],				
-				arg1View = templates.binaryArgument.clone(),
-				arg2View = templates.binaryArgument.clone(),
-				opView = templates.operator.clone();
-				
-		view.addClass('binary op-'+op)
-		    .append(arg1View)
-		    .append(opView)
-			  .append(arg2View);
-		opView.text(self.operators(op).label);
-		arg1View.append(self.render(arg1, target));
-		arg2View.append(self.render(arg2, target));
+    
+    function genericBinaryOpRenderer(ex, target){
+        var self = this,
+            op = ex.op,
+            view = templates.expression.clone(),
+            arg1 = ex.args[0],
+            arg2 = ex.args[1],                
+            arg1View = templates.binaryArgument.clone(),
+            arg2View = templates.binaryArgument.clone(),
+            opView = templates.operator.clone();
+                
+        view.addClass('binary op-'+op)
+            .append(arg1View)
+            .append(opView)
+              .append(arg2View);
+        opView.text(self.operators(op).label);
+        arg1View.append(self.render(arg1, target));
+        arg2View.append(self.render(arg2, target));
 
-		return view;	
-	};
-	
-	function getRenderer(opName){
-		return renderers[opName];
-	}
-	
-	return {
-		getRenderer:getRenderer,
-		target:TARGET
-	};
+        return view;    
+    };
+    
+    function getRenderer(opName){
+        return renderers[opName];
+    }
+    
+    return {
+        getRenderer:getRenderer,
+        target:TARGET
+    };
 });
--- js/start.js
+++ js/start.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
   $.getJSON('./json/exp1.json')
   .done(function(data){
-		parseExpr(data);
-	})
+        parseExpr(data);
+    })
   .fail(function(jqXHR, textStatus, errorThrown) {
     console.log( "error: " + errorThrown);
   });
-	
-	
+    
+    
 });
 
 var context = net.ntxt.expressions.context();
@@ -15,22 +15,22 @@ context.addRenderers(net.ntxt.expressions.renderers.html());
 context.addRenderers(net.ntxt.expressions.renderers.plaintext());
 
 function parseExpr(data){
-	try{
-		var e = context.fromJsonStruct(data);
-		var cfg = {amount:21, name:'Olaboga'};
-		var result = context.context(cfg).evaluate(e);
-		var view1 = context.render(e, 'html');
-		var view2 = context.render(e, 'plaintext');
-		$('.evalResult').html(result ? "true" : "false");
-		$('.view1').html(view1);
-		$('.view2').html(view2);
-		$('.expression').mouseenter(showContextMenu);
-	}catch(e){
-		$('.error').html(e);
-	}
+    try{
+        var e = context.fromJsonStruct(data);
+        var cfg = {amount:21, name:'Olaboga'};
+        var result = context.context(cfg).evaluate(e);
+        var view1 = context.render(e, 'html');
+        var view2 = context.render(e, 'plaintext');
+        $('.evalResult').html(result ? "true" : "false");
+        $('.view1').html(view1);
+        $('.view2').html(view2);
+        $('.expression').mouseenter(showContextMenu);
+    }catch(e){
+        $('.error').html(e);
+    }
 }
 
 function showContextMenu(){
-	var pos = $(this).offset();
-	$('.contextMenu').animate({left:pos.left + $(this).width(), top:pos.top});
+    var pos = $(this).offset();
+    $('.contextMenu').animate({left:pos.left + $(this).width(), top:pos.top});
 }
